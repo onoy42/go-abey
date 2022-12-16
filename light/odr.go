2bfb943c20d097cb846473ee1b3dcb710557b5dd
onoy42 <ibm.dev9471@gmail.com>
2022-12-16 17:17:39 +0800

    update the light client

--- light/odr.go
+++ light/odr.go
@@ -41,14 +41,72 @@ var ErrNoPeers = errors.New("no suitable peers available")
 // OdrBackend is an interface to a backend service that handles ODR retrievals type
 type OdrBackend interface {
 	Database() abeydb.Database
-	ChtIndexer() *snailchain.ChainIndexer
+	ChtIndexer() *core.ChainIndexer
+	BloomTrieIndexer() *core.ChainIndexer
+	BloomIndexer() *core.ChainIndexer
 	Retrieve(ctx context.Context, req OdrRequest) error
-	IndexerConfig() *public.IndexerConfig
+	IndexerConfig() *IndexerConfig
 }
 
 // OdrRequest is an interface for retrieval requests
 type OdrRequest interface {
 	StoreResult(db abeydb.Database)
+}
+
+// TrieID identifies a state or account storage trie
+type TrieID struct {
+	BlockHash, Root common.Hash
+	BlockNumber     uint64
+	AccKey          []byte
+}
+
+// StateTrieID returns a TrieID for a state trie belonging to a certain block
+// header.
+func StateTrieID(header *types.Header) *TrieID {
+	return &TrieID{
+		BlockHash:   header.Hash(),
+		BlockNumber: header.Number.Uint64(),
+		AccKey:      nil,
+		Root:        header.Root,
+	}
+}
+
+// StorageTrieID returns a TrieID for a contract storage trie at a given account
+// of a given state trie. It also requires the root hash of the trie for
+// checking Merkle proofs.
+func StorageTrieID(state *TrieID, addrHash, root common.Hash) *TrieID {
+	return &TrieID{
+		BlockHash:   state.BlockHash,
+		BlockNumber: state.BlockNumber,
+		AccKey:      addrHash[:],
+		Root:        root,
+	}
+}
+
+// TrieRequest is the ODR request type for state/storage trie entries
+type TrieRequest struct {
+	OdrRequest
+	Id    *TrieID
+	Key   []byte
+	Proof *NodeSet
+}
+
+// StoreResult stores the retrieved data in local database
+func (req *TrieRequest) StoreResult(db ethdb.Database) {
+	req.Proof.Store(db)
+}
+
+// CodeRequest is the ODR request type for retrieving contract code
+type CodeRequest struct {
+	OdrRequest
+	Id   *TrieID // references storage trie of the account
+	Hash common.Hash
+	Data []byte
+}
+
+// StoreResult stores the retrieved data in local database
+func (req *CodeRequest) StoreResult(db ethdb.Database) {
+	db.Put(req.Hash[:], req.Data)
 }
 
 // BlockRequest is the ODR request type for retrieving block bodies
@@ -64,65 +122,59 @@ func (req *BlockRequest) StoreResult(db abeydb.Database) {
 	rawdb.WriteBodyRLP(db, req.Hash, req.Number, req.Rlp)
 }
 
+// ReceiptsRequest is the ODR request type for retrieving block bodies
+type ReceiptsRequest struct {
+	OdrRequest
+	Hash     common.Hash
+	Number   uint64
+	Receipts types.Receipts
+}
+
+// StoreResult stores the retrieved data in local database
+func (req *ReceiptsRequest) StoreResult(db ethdb.Database) {
+	rawdb.WriteReceipts(db, req.Hash, req.Number, req.Receipts)
+}
+
 // ChtRequest is the ODR request type for state/storage trie entries
 type ChtRequest struct {
 	OdrRequest
-	Untrusted        bool   // Indicator whether the result retrieved is trusted or not
-	PeerId           string // The specified peer id from which to retrieve data.
-	Config           *public.IndexerConfig
+	Config           *IndexerConfig
 	ChtNum, BlockNum uint64
 	ChtRoot          common.Hash
-	Header           *types.SnailHeader
+	Header           *types.Header
 	Td               *big.Int
-	Proof            *public.NodeSet
-	Headers          []*types.SnailHeader
-	Start            bool
-	FHeader          *types.Header
-	Dataset          [][]byte
-	DatasetRoot      common.Hash
+	Proof            *NodeSet
 }
 
 // StoreResult stores the retrieved data in local database
 func (req *ChtRequest) StoreResult(db abeydb.Database) {
 	hash, num := req.Header.Hash(), req.Header.Number.Uint64()
 
-	if !req.Untrusted {
-		rawdb.WriteHeader(db, req.Header)
-		rawdb.WriteTd(db, hash, num, req.Td)
-		rawdb.WriteCanonicalHash(db, hash, num)
-	}
-	rawdb.WriteLightCheckPoint(db, num)
-	if len(req.Headers) > 0 {
-		for _, head := range req.Headers {
-			rawdb.WriteHeader(db, head)
-		}
-	}
-
-	if req.Start {
-		epoch := uint64((num - 1) / minerva.UPDATABLOCKLENGTH)
-		if count := len(req.Dataset); count > minerva.STARTUPDATENUM {
-			rawdb.WriteLastDataSet(db, epoch-1, req.Dataset[:minerva.STARTUPDATENUM])
-			rawdb.WriteLastDataSet(db, epoch, req.Dataset[minerva.STARTUPDATENUM:])
-		} else {
-			rawdb.WriteLastDataSet(db, epoch, req.Dataset)
-		}
-	}
-
-	fhash, fnum := req.FHeader.Hash(), req.FHeader.Number.Uint64()
-	fastDB.WriteHeader(db, req.FHeader)
-	fastDB.WriteCanonicalHash(db, fhash, fnum)
-	fastDB.WriteHeadHeaderHash(db, fhash)
+	rawdb.WriteHeader(db, req.Header)
+	rawdb.WriteTd(db, hash, num, req.Td)
+	rawdb.WriteCanonicalHash(db, hash, num)
 }
 
-// BlockRequest is the ODR request type for retrieving block bodies
-type FruitRequest struct {
+// BloomRequest is the ODR request type for retrieving bloom filters from a CHT structure
+type BloomRequest struct {
 	OdrRequest
-	Hash   common.Hash
-	Number uint64
-	Rlp    []byte
+	Config           *IndexerConfig
+	BloomTrieNum     uint64
+	BitIdx           uint
+	SectionIndexList []uint64
+	BloomTrieRoot    common.Hash
+	BloomBits        [][]byte
+	Proofs           *NodeSet
 }
 
 // StoreResult stores the retrieved data in local database
-func (req *FruitRequest) StoreResult(db abeydb.Database) {
-	rawdb.WriteBodyRLP(db, req.Hash, req.Number, req.Rlp)
+func (req *BloomRequest) StoreResult(db ethdb.Database) {
+	for i, sectionIdx := range req.SectionIndexList {
+		sectionHead := rawdb.ReadCanonicalHash(db, (sectionIdx+1)*req.Config.BloomTrieSize-1)
+		// if we don't have the canonical hash stored for this section head number, we'll still store it under
+		// a key with a zero sectionHead. GetBloomBits will look there too if we still don't have the canonical
+		// hash. In the unlikely case we've retrieved the section head hash since then, we'll just retrieve the
+		// bit vector again from the network.
+		rawdb.WriteBloomBits(db, req.BitIdx, sectionIdx, sectionHead, req.BloomBits[i])
+	}
 }
