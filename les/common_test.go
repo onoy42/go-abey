bc6a26c821d4634d240273c4cc1f42792d7c864f
onoy42 <ibm.dev9471@gmail.com>
2023-02-21 20:55:01 +0800

    add the test for the epoch transfer

--- les/common_test.go
+++ les/common_test.go
@@ -1,1 +1,21 @@
 package les
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_01(t *testing.T) {
+	begin, end, id := LesFirstEpoch()
+	fmt.Println(begin, end, id)
+	height := uint64(9100000)
+	begin, end, id = LesEpochFromHeight(height)
+	fmt.Println(begin, end, id)
+}
+
+func Test_02(t *testing.T) {
+	for i := uint64(0); i < 100; i++ {
+		begin, end := LesEpochToHeight(i)
+		fmt.Println("epoch", i, "begin", begin, "end", end)
+	}
+}
